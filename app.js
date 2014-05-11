0155398d999398d66a0497e52aa85248119c620b
eder-toledo <me890506@gmail.com>
2014-05-10 20:59:55 -0500

    He comenzado con el diseño, ademas se han agregado estilos y js que seran usados.

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var demo = require('./routes/demo');
 
 var app = express();
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/demo', demo);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
